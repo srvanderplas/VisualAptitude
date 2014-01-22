998aff9e766a49336bccec69a71653df6833b0d8
Susan VanderPlas <srvanderplas@gmail.com>
2014-01-22 15:52:01 -0600

    Outliers in separate group, only generate outlier if n.outliers>0

--- Code/GenerateData.R
+++ Code/GenerateData.R
@@ -2,12 +2,16 @@ linear.trend <- function(n=30, a=sample(c(-1, 1), 1)*rnorm(1, 3), b=rnorm(1), n.
   df <- data.frame(x=seq(-n/10, n/10, length.out=n))
   df$y <- a*df$x+b+rnorm(n)
   df$outlier <- FALSE
-  # select outlier from the first 1/5 of the dataset or the last 1/5 of the dataset
-  outlier.vals <- c(1:n)
-  outlier.vals <- outlier.vals[outlier.vals<=n/5 | outlier.vals>=4*n/5]
-  outlier <- sample(outlier.vals, 1)
-  df$outlier[outlier] <- TRUE
-  df$y[outlier] <- df$y[outlier]-sign(df$x[outlier])*3*a
-  df$group <- as.numeric((df$x+rnorm(n))>0)+1
+  df$group <- as.numeric((df$x+rnorm(n))>0)+1   
+  if(n.outliers>0){
+    # select outlier from the first 1/5 of the dataset or the last 1/5 of the dataset
+    outlier.vals <- c(1:n)
+    outlier.vals <- outlier.vals[outlier.vals<=n/5 | outlier.vals>=4*n/5]
+    outlier <- sample(outlier.vals, n.outliers)
+    df$outlier[outlier] <- TRUE
+    df$y[outlier] <- df$y[outlier]-sign(df$x[outlier])*3*a
+    df$group[outlier] <- 3
+  }
+
   return(df)
 }
