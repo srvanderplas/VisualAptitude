e1452adbf74761b47a4c8a349ddd34cabe3cd33d
Susan VanderPlas <srvanderplas@gmail.com>
2014-01-22 16:42:06 -0600

    Changing outlier generation and std. errors

--- Code/GenerateData.R
+++ Code/GenerateData.R
@@ -1,16 +1,20 @@
 require(plyr)
 linear.trend <- function(n=30, a=sample(c(-1, 1), 1)*rnorm(1, 3), b=rnorm(1), n.outliers=0){
+  sd=sqrt(n/5)
   df <- data.frame(x=seq(-n/10, n/10, length.out=n))
-  df$y <- a*df$x+b+rnorm(n, sd=sqrt(2*abs(a)))
+  df$y <- rnorm(n, a*df$x+b+rnorm(n, sd=sd))
   df$outlier <- FALSE
   df$group <- as.numeric((df$x+rnorm(n, sd=1/sqrt(n/10)))>0)+1   
   if(n.outliers>0){
     # select outlier from the first 1/5 of the dataset or the last 1/5 of the dataset
-    outlier.vals <- c(1:n)
-    outlier.vals <- outlier.vals[outlier.vals<=n/5 | outlier.vals>=4*n/5]
+    idx <- c(1:n)
+    cutoff <- sample(c("low", "high"), 1)
+    outlier.vals <- idx[(cutoff=="low" & idx<=n/5) | (cutoff=="high" & idx>=4*n/5)]
     outlier <- sample(outlier.vals, n.outliers)
+    replace.vals <- idx[(cutoff!="low" & idx<=n/5) | (cutoff!="high" & idx>=4*n/5)]
+    replace <- sample(replace.vals, n.outliers)
     df$outlier[outlier] <- TRUE
-    df$y[outlier] <- df$y[outlier]-sign(df$x[outlier])*3*a
+    df$y[outlier] <- rnorm(n.outliers, df$y[replace], sd=sd/3)
     df$group[outlier] <- 3
   }
 
@@ -19,10 +23,11 @@ linear.trend <- function(n=30, a=sample(c(-1, 1), 1)*rnorm(1, 3), b=rnorm(1), n.
 
 permute.groups2 <- function(lineupdata, ngroups=3, pos=sample(1:20, 1)){
   ddply(lineupdata, .(.sample), function(df){
+    dst <- dist(df[,c("x", "y")])
     if(sum(df$.sample==pos)==0){
-      df$group.k = kmeans(df[,1:2], centers=1+nrow(dframe)/(ngroups+1))$cluster%%ngroups+1
+      df$group.k = cutree(hclust(dst, method="complete"), round(nrow(df)/(ngroups+1)+1))%%ngroups+1
     } else {
-      df$group.k = kmeans(df[,1:2], centers=ngroups)$cluster
+      df$group.k = cutree(hclust(dst, method="complete"), ngroups)
     }
     df
   })
