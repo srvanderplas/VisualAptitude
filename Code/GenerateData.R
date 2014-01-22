ef37f5673976c7d9f7e5b7698e896549872a209d
Susan VanderPlas <srvanderplas@gmail.com>
2014-01-22 15:56:09 -0600

    change std deviation of generation alg

--- Code/GenerateData.R
+++ Code/GenerateData.R
@@ -1,8 +1,8 @@
 linear.trend <- function(n=30, a=sample(c(-1, 1), 1)*rnorm(1, 3), b=rnorm(1), n.outliers=0){
   df <- data.frame(x=seq(-n/10, n/10, length.out=n))
-  df$y <- a*df$x+b+rnorm(n)
+  df$y <- a*df$x+b+rnorm(n, sd=sqrt(abs(a)/2))
   df$outlier <- FALSE
-  df$group <- as.numeric((df$x+rnorm(n))>0)+1   
+  df$group <- as.numeric((df$x+rnorm(n, sd=1/sqrt(n/10)))>0)+1   
   if(n.outliers>0){
     # select outlier from the first 1/5 of the dataset or the last 1/5 of the dataset
     outlier.vals <- c(1:n)
