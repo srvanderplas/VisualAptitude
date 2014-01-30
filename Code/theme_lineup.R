c9dddca8455f57c5a98a03de8c0c801c158a0533
Susan VanderPlas <srvanderplas@gmail.com>
2014-01-29 21:31:23 -0600

    RNW files for stimuli to generate a single file with all stimuli.

--- Code/theme_lineup.R
+++ Code/theme_lineup.R
@@ -4,5 +4,5 @@ theme_lineup <- function(base_size = 12, base_family = ""){
   theme_bw(base_size = base_size, base_family = base_family) %+replace% 
     theme(legend.position="none", 
           axis.text=element_blank(),
-#           axis.title=element_blank(),
+          axis.title=element_blank(),
           axis.ticks=element_blank())}
