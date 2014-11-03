4ac7dcf367c74078991e64ca0a2a426ec4e9dc33
Susan VanderPlas <srvanderplas@gmail.com>
2014-11-03 09:43:19 -0600

    Add STEM indicator

--- Code/Analysis.R
+++ Code/Analysis.R
@@ -7,15 +7,20 @@ ans$id <- 1:nrow(ans)
 # Fix age categories
 ans$age <- gsub("21-24", "21+", gsub("25-28", "21+", ans$age))
 
+
+ans$stem <- str_detect(ans$major1, "(Engineering)|(Biology)|([cC]hemistry)|(Statistics)|(Genetics)|(Animal Science)|(Horticulture)")
+
+ans <- ans[,c(1:19, 397, 20:396)]
+
 # Create a key that has the same structure as the answers
 key <- ans[1,]
-key[,1:19] <- NA
+key[,1:20] <- NA
 # Visual Search
-key[,20:44] <- c(17, 12, 15, 14, 8, 21, 6, 3, 21, 22, 4, 23, 9, 8, 22, 13, 2, 19, 16, 18, 7, 16, 12, 1, 7)
+key[,21:45] <- c(17, 12, 15, 14, 8, 21, 6, 3, 21, 22, 4, 23, 9, 8, 22, 13, 2, 19, 16, 18, 7, 16, 12, 1, 7)
 # Lineups 1
-key[,45:64] <- c(6, 16, 13, 10, 17, 6, 16, 13, 10, 17, 6, 16, 13, 10, 17, 6, 16, 13, 10, 17)
+key[,46:65] <- c(6, 16, 13, 10, 17, 6, 16, 13, 10, 17, 6, 16, 13, 10, 17, 6, 16, 13, 10, 17)
 # Card  Rotation
-key[,65:224] <- c("d", "s", "s", "d", "d", "s", "d", "s",  "s", "s", "s", "d", "s", "s", "s", "s", 
+key[,66:225] <- c("d", "s", "s", "d", "d", "s", "d", "s",  "s", "s", "s", "d", "s", "s", "s", "s", 
                   "s", "d", "d", "d", "s", "s", "s", "d",  "s", "s", "d", "s", "d", "d", "d", "s",
                   "d", "s", "d", "d", "s", "s", "d", "s",  "s", "d", "s", "s", "s", "s", "d", "d", 
                   "s", "d", "s", "d", "d", "s", "s", "s",  "d", "d", "s", "s", "d", "s", "d", "d", 
@@ -26,10 +31,10 @@ key[,65:224] <- c("d", "s", "s", "d", "d", "s", "d", "s",  "s", "s", "s", "d", "
                   "s", "s", "d", "d", "d", "d", "d", "s",  "s", "s", "s", "s", "d", "d", "s", "s",
                   "s", "s", "d", "d", "d", "d", "d", "s",  "s", "d", "d", "d", "s", "s", "s", "s")
 # Lineups 2
-key[,225:244] <- c(15, 15, 15,  4,  6,   15,  6, 15,  4,  6,   6,  6,  4,  4, 15,   4, 15, 15, 15, 15)
+key[,226:245] <- c(15, 15, 15,  4,  6,   15,  6, 15,  4,  6,   6,  6,  4,  4, 15,   4, 15, 15, 15, 15)
 
 # Figure Classification
-key[,245:356] <- c(2, 1, 1, 2, 1, 2, 2, 2,  2, 1, 2, 1, 2, 1, 2, 2, 
+key[,246:357] <- c(2, 1, 1, 2, 1, 2, 2, 2,  2, 1, 2, 1, 2, 1, 2, 2, 
                    1, 2, 2, 1, 2, 2, 1, 2,  2, 1, 1, 2, 2, 1, 2, 1, 
                    2, 1, 2, 1, 1, 2, 1, 1,  2, 1, 2, 1, 1, 2, 2, 1, 
                    1, 2, 1, 2, 1, 2, 1, 1,  1, 2, 2, 2, 1, 1, 2, 2, 
@@ -38,13 +43,13 @@ key[,245:356] <- c(2, 1, 1, 2, 1, 2, 2, 2,  2, 1, 2, 1, 2, 1, 2, 2,
                    2, 2, 1, 3, 3, 1, 1, 1,  3, 1, 2, 3, 1, 3, 2, 2)
 
 # Lineups 3
-key[,357:376] <- c(12, 19, 14,  4, 13, 
+key[,358:377] <- c(12, 19, 14,  4, 13, 
                     4, 14,  7,  8, 19, 
                     8, 19, 12, 18, 12, 
                     7,  7,  6,  6,  5)
 
 # Paper Folding
-key[,377:396] <- c("a", "d", "b", "d", "b", 
+key[,378:397] <- c("a", "d", "b", "d", "b", 
                    "e", "a", "c", "e", "e", 
                    "c", "b", "a", "e", "b", 
                    "a", "e", "d", "d", "c")
@@ -55,14 +60,14 @@ library(plyr)
 scored <- ddply(ans, .(id), function(j) as.numeric(j==key) + c(0, NA)[1+is.na(j)])
 names(scored) <- names(ans)
 # Transfer demographic data
-scored[,1:19] <- ans[,1:19]
+scored[,1:20] <- ans[,1:20]
 # scored[,357:376] <- NA # can't find answers for Lineup3?
-scored[,20:396] <- apply(scored[,20:396], 2, as.numeric)
+scored[,21:397] <- apply(scored[,21:397], 2, as.numeric)
 
 # Melt scored data into long format, calculate penalties, and compute scores using ddply
 library(reshape2)
 library(stringr)
-longform <- melt(scored[,c(1, 20:396)], id.vars=1)
+longform <- melt(scored[,c(1, 21:397)], id.vars=1)
 longform$testtype <- gsub("_q[0123456789]+[abcdefgh]?$", "", longform$variable)
 longform$testnum <- as.numeric(gsub("[[:alpha:]_]+", "", longform$testtype))
 longform$testtype <- gsub("[[:digit:]]", "", longform$testtype)
@@ -104,7 +109,7 @@ longform.sum$value <- with(longform.sum, pos.pts-neg.pts)
 
 # Cast back to wide-ish form with just test totals
 ans.summary <- dcast(longform.sum, id~testtype, value.var="value", na.rm=TRUE)
-ans.summary <- merge(ans[,1:19], ans.summary)
+ans.summary <- merge(ans[,1:20], ans.summary)
 pct.ans <- dcast(longform.sum, id~testtype, value.var="pct.answered")
 
 # Cast unscaled version
@@ -115,7 +120,7 @@ pct.ans <- dcast(longform.sum, id~testtype, value.var="pct.answered")
 ans.summary$vidgame_hrs_factor <- ordered(factor(rowSums(sapply(c(-1, 0, 1.99, 4.99), function(i) ans$vidgame_hrs>i)), labels=c("0", "[1, 2)", "[2, 5)", "5+"), levels=1:4))
 
 # Create plot-able data frame with test results
-lineup.summary <- melt(ans.summary, id.vars=c(1:19, 23, 25))
+lineup.summary <- melt(ans.summary, id.vars=c(1:20, 24, 26))
 
 # Create plot-able data frame with demographic data
-lineup.summary.categorical <- suppressWarnings(melt(ans.summary[,c(1:3, 12:17, 19, 23, 25)], id.vars=c("id", "lineup")))
+lineup.summary.categorical <- suppressWarnings(melt(ans.summary[,c(1:3, 12:17, 19, 20, 24, 26)], id.vars=c("id", "lineup")))
